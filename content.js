b5438825c5b4ec887cb976d40495c0e8a66b2e11
Will Kaczmarek <kaczmarekwill@gmail.com>
2017-11-01 14:54:59 -0400

    Added prompt to get desired recipe scale

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+let scalingFactor = 1;
+let origServings = 1;
+let newServings = 1;
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
@@ -7,5 +11,36 @@ chrome.runtime.onMessage.addListener(
 );
 
 function getRecipeScale() {
-  var origServings = prompt('Enter the number of servings in the original recipee. Enter 0 to scale by multiplication.');
+  origServings = prompt('Enter the number of servings in the original recipe. Enter 0 to scale by multiplication.');
+
+  if (origServings > 0) {
+    newServings = prompt('Enter the desired number of servings');
+    if (newServings > 0) {
+      scalingFactor = newServings/origServings;  
+    } else {
+      invalidInput();
+    }
+    
+  } else if (origServings == 0) {
+    scalingFactor = prompt('Enter the multiple you would like to scale the recipe by');
+    if (scalingFactor <= 0 || isNaN(scalingFactor)) {
+      invalidInput();
+    } 
+
+  } else {
+    invalidInput();
+  }
+  
+  if (scalingFactor != 1) {
+    scaleRecipe();
+  }
+}
+
+function invalidInput() {
+  alert('Invalid input');
+  scalingFactor = 1;
+}
+
+function scaleRecipe() {
+  alert('Recipe Scaled');
 }
